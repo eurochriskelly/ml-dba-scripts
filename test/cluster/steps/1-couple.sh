4aecd83c2bdd2c64ec3e51bc2a2cc834011e5dd7
Chris Kelly <eurochriskelly@gmail.com>
2024-04-16 18:46:27 +0200

    Switch from docker to official marklogic kubernetes image

--- test/cluster/steps/1-couple.sh
+++ test/cluster/steps/1-couple.sh
@@ -1,9 +1,5 @@
 #!/bin.bash
 # Run from top-level directory
-if [ ! -d .git ]; then
-    echo "Please run this script from the top-level directory of the project"
-    exit 1
-fi
-
-source test/cluster/local-docker-env.sh
-bash scripts/cluster/couple.sh --run 
+cd /tmp
+source ./local-kub-env.sh
+bash ./couple.sh --run 
