c471c45ac338cd21001b67ba9923a729af5ea5c7
Chris Kelly <eurochriskelly@gmail.com>
2024-11-07 22:40:45 +0100

    Add modules database as a parameter

--- scripts/prep_eval.sh
+++ scripts/prep_eval.sh
@@ -2,12 +2,14 @@
 
 # Default values
 database="Documents"
+modules=""
 
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     --xqy) input_file="$2"; shift ;;
     --database) database="$2"; shift ;;
+    --modules) modules="$2"; shift ;;
     *) echo "Unknown parameter: $1"; exit 1 ;;
   esac
   shift
@@ -15,7 +17,7 @@ done
 
 # Check if input file is provided
 if [ -z "$input_file" ]; then
-  echo "Usage: bash prep.sh --xqy foo.xqy [--database database_name]"
+  echo "Usage: bash prep.sh --xqy foo.xqy [--database database_name] [--modules module_name]"
   exit 1
 fi
 
@@ -26,7 +28,7 @@ output_file="_${filename}.xqy"
 # Read the contents of the input file
 contents=$(<"$input_file")
 
-# Create the output XQuery file with contents and database embedded in the template
+# Create the output XQuery file with contents, database, and modules embedded in the template
 cat << EOF > "$output_file"
 xquery version "1.0-ml";
 
@@ -34,7 +36,8 @@ xdmp:eval(<root><![CDATA[
 $contents
 ]]></root>//text(), (), <options xmlns="xdmp:eval">
   <database>{xdmp:database("$database")}</database>
+  <modules>{xdmp:database("$modules")}</modules>
 </options>)
 EOF
 
-echo "Created $output_file with embedded contents from $input_file and database set to $database"
+echo "Created $output_file with embedded contents from $input_file, database set to $database, and modules set to $modules"
